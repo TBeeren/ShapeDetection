966549ebae93f9b1374042c06e8714191ecf63c6
Stijn krabbenborg <S.Krabbenborg@student.han.nl>
2020-02-18 11:02:16 +0100

    added default main

---
+++ Code/main.cpp
@@ -0,0 +1,20 @@
+/**
+  * @file CColour.h
+  * @author Tim Beeren (T.Beeren@student.han.nl) Stijn Krabbenborg (S.Krabbenborg@student.han.nl) Evren Kilic (ET.Kilic@student.han.nl)
+  * @brief 
+  * @version 0.1
+  * @date 07-02-2020
+  * 
+  * @copyright Copyright (c) 2020
+  * 
+  */
+ 
+ #include "CFeatureExtraction.h"
+ 
+ #include "opencv2/opencv.hpp"
+ 
+ #include <iostream>
+ int main(int argc, char const *argv[])
+ {
+     return 0;
+ }
